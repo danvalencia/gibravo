b89dc1a79e4a6d7538ec54a2193ad89ca9c98435
Daniel Valencia <danvalencia@gmail.com>
2010-08-01 19:11:33 -0500

    Adding fadeIn to images.

--- javascript/image.js
+++ javascript/image.js
@@ -3,7 +3,8 @@ Image={}
 Image.selectImage = function(imageName, imgThumb)
 {
 		$(".thumb").removeClass("thumb_selected");
-		$(imgThumb).addClass("thumb_selected")
-		$(imgThumb).sty
-		$('#_image').attr('src', imageName);
+		$(imgThumb).addClass("thumb_selected");
+		$('#_image').fadeOut("slow", function(){
+				$('#_image').fadeIn("slow").attr('src', imageName);
+		});
 }
