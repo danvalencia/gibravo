01a5e5db52405ef54e270db685c4c0c15eae7610
Daniel Valencia <danvalencia@gmail.com>
2010-08-16 23:00:38 -0500

    + Under Construction.

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<title>gibravo.com</title>
+		<style type="text/css" media="screen">
+			html,body{
+				font-family: "Helvetica Black", "Helvetica", sans-serif;
+				font-size:12px;
+				background-color: #2D2D2D;
+				margin:0px;
+				padding:0px;
+				
+			}
+			
+			#container
+			{
+				width: 600px;
+				margin: auto;
+				margin-top : 200px;
+				text-align: center;
+				position : relative;
+			}			
+		</style>
+	</head>
+	<body>
+		<div id="container" >
+			<img src="/images/construction.png" ></img>
+		</div>
+	</body>
+</html>
